ecdc03f2fbde6d5149c91728626e04457162439d
Alexander <bellal89@mail.ru>
2012-07-09 18:56:34 +0600

    default gitignore generated

---
+++ src/cqa_medical/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cqa_medical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
