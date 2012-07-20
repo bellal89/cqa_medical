2c3bcea11a2a704fbba334e94110c36f6c8d2793
Kuznetsov Artem <whoisnexta@gmail.com>
2012-07-20 18:49:19 +0600

    AOTLemmatizator

---
+++ src/cqa_medical/DataInput/Stemmers/AOTLemmatizer/AOTLemmatizer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using cqa_medical.DataInput.Stemmers;
+
+namespace cqa_medical.DataInput.AOTLemmatizer
+{
+	class AOTLemmatizer : IStemmer
+	{
+		private readonly LEMMATIZERLib.ILemmatizer lemmatizerRu;
+		/// <summary>
+		/// this needs 32 bit version of project
+		/// You can change it in project properties
+		/// </summary>
+		public AOTLemmatizer()
+		{
+			lemmatizerRu = new LEMMATIZERLib.LemmatizerRussian();
+			lemmatizerRu.LoadDictionariesRegistry();
+		}
+
+		public string Stem(string s)
+		{
+			LEMMATIZERLib.IParadigmCollection piParadigmCollection = lemmatizerRu.CreateParadigmCollectionFromForm(s, 0, 0);
+			if (piParadigmCollection.Count == 0) 
+				return s;
+			return piParadigmCollection[0].Norm;
+		}
+
+
+
+	}
+}
--- src/cqa_medical/Program.cs
+++ src/cqa_medical/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using cqa_medical.DataInput;
 using cqa_medical.Statistics;
 using cqa_medical.BodyAnalisys;
+using cqa_medical.DataInput.AOTLemmatizer;
 
 namespace cqa_medical
 {
@@ -15,23 +16,27 @@ namespace cqa_medical
 		const string QuestionsFileName = "../../Files/qst_25.csv";
 		const string AnswersFileName = "../../Files/ans_25.csv";
 
+		const string QuestionsTestFileName = "../../Files/QuestionsTest.csv";
+		const string AnswersTestFileName = "../../Files/AnswersTest.csv";
+
 		private static QuestionList ParseAndStem()
 		{
-			var questionList = Parse();
+			var questionList = Parse(QuestionsFileName, AnswersFileName);
 			return questionList.StemIt();
 		}
 
-    	private static QuestionList Parse()
+		private static QuestionList Parse(String questionsFileName, string answersFileName)
     	{
     		var questionList = new QuestionList();
 
     		var start = DateTime.Now;
-    		var parser = new Parser(QuestionsFileName, AnswersFileName);
+			var parser = new Parser(questionsFileName, answersFileName);
     		parser.Parse(questionList.AddQuestion, questionList.AddAnswer);
 
 			Console.WriteLine(String.Format("Parsing Completed in {0}", (DateTime.Now - start).TotalSeconds));
     		return questionList;
     	}
+		
 
     	private static void BodyPartsWork()
 		{
@@ -45,21 +50,18 @@ namespace cqa_medical
 			File.WriteAllText("2.txt", newBody.ToString(allQuestionsCount), Encoding.UTF8);
 		}
 
-        static void Main(string[] args)
-        {
-			BodyPartsWork();
-//			var questionList = Parse();
-//			var statistics = new Statistics.Statistics(questionList);
-//			File.WriteAllText(
-//				"WordIntensityDistributionInWeeks.txt",
-//				statistics.WordIntensityDistributionInWeeks(new string[]{"грипп", "ОРВИ"}).ToStringNormal()
-//			);
-//			File.WriteAllText(
-//				"WordQuotientDistributionInWeeks.txt",
-//				statistics.WordQuotientDistributionInWeeks(new string[] { "грипп", "ОРВИ" }).ToStringNormal()
-//			);
-        }
-
-    	
+		static void Main(string[] args)
+		{
+			//var questionList = Parse(QuestionsFileName, AnswersFileName);
+			//var statistics = new Statistics.Statistics(questionList);
+			//File.WriteAllText(
+			//    "WordIntensityDistributionInWeeks.txt",
+			//    statistics.WordIntensityDistributionInWeeks(new string[] { "грипп", "ОРВИ" }).ToStringNormal()
+			//);
+			//File.WriteAllText(
+			//    "WordQuotientDistributionInWeeks.txt",
+			//    statistics.WordQuotientDistributionInWeeks(new string[] { "грипп", "ОРВИ" }).ToStringNormal()
+			//);
+		}
     }
 }
