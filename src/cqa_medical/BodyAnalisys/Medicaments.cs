399eaae134c350d4e800230d7d1b58c7f5b85c1f
Alexander <bellal89@mail.ru>
2012-07-24 17:49:40 +0600

    Medicaments retrieval logic works. The first word of every medicament, pruning by Count = 85.

--- src/cqa_medical/BodyAnalisys/Medicaments.cs
+++ src/cqa_medical/BodyAnalisys/Medicaments.cs
@@ -3,33 +3,77 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
+using cqa_medical.DataInput.Stemmers;
+using cqa_medical.DataInput.Stemmers.MyStemmer;
 
 namespace cqa_medical.BodyAnalisys
 {
 	class Medicaments
 	{
-		private Dictionary<string, List<int>> invertedIndex = new Dictionary<string, List<int>>(); 
-
-		public Medicaments()
+		private readonly Dictionary<string, HashSet<int>> invertedIndex = new Dictionary<string, HashSet<int>>();
+		private readonly IStemmer stemmer;
+		
+		public Medicaments(IStemmer stemmer, string medicamentsFileName)
 		{
-			FromFile();
+			this.stemmer = stemmer;
+			invertedIndex = LoadIndexFromFile(medicamentsFileName);
 		}
 
-		private void FromFile()
+		private Dictionary<string, HashSet<int>> LoadIndexFromFile(string fileName)
 		{
+			var index = new Dictionary<string, HashSet<int>>();
 			var i = 1;
-			foreach (var line in File.ReadAllLines(Program.MedicamentsFileName))
+			foreach (var parts in File.ReadAllLines(fileName)
+									  .Distinct()
+									  .Select(line => line.Split(new[] {'\t'}, StringSplitOptions.RemoveEmptyEntries))
+									  .Where(item => item.Length > 1))
 			{
-				var parts = line.Split(new[] {'\t'}, StringSplitOptions.RemoveEmptyEntries);
-				addToIndex(parts[0]);
-				addToIndex(parts[1]);
+				AddToIndex(index, parts[0], i);
+				AddToIndex(index, parts[1], i);
+				i++;
 			}
-			
+			return Filter(index);
 		}
 
-		private void addToIndex(string p0)
+		private static Dictionary<string, HashSet<int>> Filter(Dictionary<string, HashSet<int>> index)
 		{
-			throw new NotImplementedException();
+			return index.Where(item => item.Value.Count <= 85).ToDictionary(item => item.Key, item => item.Value);
+		}
+
+		private void AddToIndex(IDictionary<string, HashSet<int>> index, string medicamentFullName, int medicamentId)
+		{
+			var words = medicamentFullName.ToLower().Split(new[] { ' ', '®', '+', '-', '—', '~', '[', ']', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+			
+			if (words.Length == 0) return;
+			
+			var stemmedWord = stemmer.Stem(words[0]);
+			if (!index.ContainsKey(stemmedWord))
+			{
+				index.Add(stemmedWord, new HashSet<int>());
+			}
+			index[stemmedWord].Add(medicamentId);
+		}
+
+		public override string ToString()
+		{
+			return String.Join("\n",
+			            invertedIndex.OrderByDescending(item => item.Value.Count).Select(
+			            	item => item.Value.Count + "\t" + item.Key + "\t" + String.Join(" ", item.Value.OrderBy(id => id))));
+		}
+	}
+
+	[TestFixture]
+	public class MedicamentsTest
+	{
+		[Test]
+		public void CreationTest()
+		{
+			var medicaments = new Medicaments(
+				new MyStemmer(new Vocabulary(Program.QuestionsFileName, Program.AnswersFileName)),
+				Program.MedicamentsFileName);
+			Console.WriteLine(medicaments);
+			File.WriteAllText("MedOutput2.txt", medicaments.ToString());
 		}
 	}
 }
