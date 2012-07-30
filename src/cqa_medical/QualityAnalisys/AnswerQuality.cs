18ba7dbe02afd5115d44f9a9b60f3d47fa176a98
Alexander <bellal89@mail.ru>
2012-07-30 18:17:03 +0600

    User|Answer Quality classes added. Formulas roughly fitted.

---
+++ src/cqa_medical/QualityAnalisys/AnswerQuality.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using cqa_medical.DataInput;
+
+namespace cqa_medical.QualityAnalisys
+{
+	class AnswerQuality
+	{
+		private readonly UserQuality userQuality;
+		private readonly QuestionList questionList;
+		private readonly Dictionary<long, Answer> answers = new Dictionary<long, Answer>();
+
+		public AnswerQuality(QuestionList questionList)
+		{
+			this.questionList = questionList;
+			userQuality = new UserQuality(questionList);
+			foreach (var answer in questionList.GetAllAnswers().Where(answer => !answers.ContainsKey(answer.Id)))
+			{
+				answers.Add(answer.Id, answer);
+			}
+		}
+
+		public Dictionary<long, Answer> GetAnswers()
+		{
+			return answers;
+		}
+
+		public float GetAnswerQuality(long id)
+		{
+			if (!answers.ContainsKey(id)) return 0;
+			var result = userQuality.GetUserQuality(answers[id].AuthorEmail) + 200*answers[id].IsBest;
+			if (questionList.GetQuestion(answers[id].QuestionId).Category == "doctor")
+				result += 2000;
+			return result;
+		}
+	}
+	
+	[TestFixture]
+	public class AnswersQualityTest
+	{
+		[Test]
+		public void TestAnswersQuality()
+		{
+			var answersQuality = new AnswerQuality(Program.DefaultQuestionList);
+			SaveSortedUserQuality(answersQuality, answersQuality.GetAnswerQuality);
+		}
+
+		private static void SaveSortedUserQuality(AnswerQuality answerQuality, Func<long, float> func)
+		{
+			var quality =
+				answerQuality.GetAnswers().OrderByDescending(a => func(a.Key)).Select(
+					a => a.Value.QuestionId + "\t" + a.Key + "\t" + func(a.Key) + "\t" + Program.DefaultQuestionList.GetQuestion(a.Value.QuestionId).Category);
+			File.WriteAllText("Answers_" + func.Method.Name, String.Join("\n", quality));
+		}
+	}
+}
---
+++ src/cqa_medical/QualityAnalisys/UserQuality.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using cqa_medical.DataInput;
+
+namespace cqa_medical.QualityAnalisys
+{
+	class UserQuality
+	{
+		internal class UserInfo
+		{
+			public int Rating { get; set; }
+			public float Efficiency { get; set; }
+			public int NumberOfAnswers { get; set; }
+			public DateTime LastAnswerDate { get; set; }
+		}
+
+		private readonly QuestionList questionList;
+
+		private readonly Dictionary<string, UserInfo> userInfos = new Dictionary<string, UserInfo>();
+
+		public UserQuality(QuestionList questionList)
+		{
+			this.questionList = questionList;
+			userInfos = CalculateUserInfos();
+		}
+
+		private Dictionary<string, UserInfo> CalculateUserInfos()
+		{
+			var infos = new Dictionary<string, UserInfo>();
+			foreach (var answer in questionList.GetAllAnswers())
+			{
+				if (!infos.ContainsKey(answer.AuthorEmail))
+				{
+					infos.Add(answer.AuthorEmail, new UserInfo
+					                                  	{
+					                                  		Efficiency = answer.AuthorEfficiency,
+					                                  		Rating = answer.AuthorRating,
+					                                  		NumberOfAnswers = 1,
+					                                  		LastAnswerDate = answer.DateAdded
+					                                  	});
+				}
+				else
+				{
+					infos[answer.AuthorEmail].NumberOfAnswers++;
+					if (answer.DateAdded > infos[answer.AuthorEmail].LastAnswerDate)
+					{
+						infos[answer.AuthorEmail].Efficiency = answer.AuthorEfficiency;
+						infos[answer.AuthorEmail].Rating = answer.AuthorRating;
+						infos[answer.AuthorEmail].LastAnswerDate = answer.DateAdded;
+					}
+				}
+			}
+			return infos;
+		}
+
+		public Dictionary<string, UserInfo> GetUserInfos()
+		{
+			return userInfos;
+		}
+
+		public float GetUserQuality(string userEmail)
+		{
+			if (!userInfos.ContainsKey(userEmail))
+				return 0;
+
+			return userInfos[userEmail].Efficiency * userInfos[userEmail].NumberOfAnswers;
+		}
+	}
+
+	[TestFixture]
+	public class UserQualityTest
+	{
+		[Test]
+		public void TestDifferentFuncs()
+		{
+			var userQuality = new UserQuality(Program.DefaultQuestionList);
+			SaveSortedUserQuality(userQuality, userQuality.GetUserQuality);
+		}
+
+		private static void SaveSortedUserQuality(UserQuality userQuality, Func<string, float> func)
+		{
+			var quality = userQuality.GetUserInfos().Keys.OrderByDescending(func).ToDictionary(user => user, func);
+			File.WriteAllText("Users_" + func.Method.Name, String.Join("\n", quality.Select(entry => entry.Key + "\t" + entry.Value)));
+		}
+	}
+}
