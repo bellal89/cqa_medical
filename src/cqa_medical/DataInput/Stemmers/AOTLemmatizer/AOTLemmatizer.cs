4ac620c8c96c7af2b59f8beaf31db90427a1d249
Kuznetsov Artem <whoisnexta@gmail.com>
2012-08-29 11:22:36 +0600

    Lemmatizers modify

--- src/cqa_medical/DataInput/Stemmers/AOTLemmatizer/AOTLemmatizer.cs
+++ src/cqa_medical/DataInput/Stemmers/AOTLemmatizer/AOTLemmatizer.cs
@@ -1,26 +1,45 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using LEMMATIZERLib;
+using NUnit.Framework;
 
 namespace cqa_medical.DataInput.Stemmers.AOTLemmatizer
 {
 	class AOTLemmatizer : IStemmer
 	{
-		private readonly LEMMATIZERLib.ILemmatizer lemmatizerRu;
+		private readonly ILemmatizer lemmatizerRu;
 		/// <summary>
 		/// this needs 32 bit version of project
 		/// You can change it in project properties
 		/// </summary>
 		public AOTLemmatizer()
 		{
-			lemmatizerRu = new LEMMATIZERLib.LemmatizerRussian();
+			lemmatizerRu = new LemmatizerRussian();
 			lemmatizerRu.LoadDictionariesRegistry();
 		}
 
 		public string Stem(string word)
 		{
-			LEMMATIZERLib.IParadigmCollection piParadigmCollection = lemmatizerRu.CreateParadigmCollectionFromForm(word, 1, 1);
-			if (piParadigmCollection.Count == 0) 
-				return word;
-			return piParadigmCollection[0].Norm.ToLower();
+			return GetAllNorms(word).First();
+		}
+		public IEnumerable<string> GetAllNorms(string word)
+		{
+			IParadigmCollection piParadigmCollection = lemmatizerRu.CreateParadigmCollectionFromForm(word, 0, 0);
+			var len = piParadigmCollection.Count;
+			for (int i = 0; i < len; ++i )
+				yield return piParadigmCollection[i].Norm.ToLower();
+		}
+		public IEnumerable<string> GetAllForms(string word)
+		{
+			IParadigmCollection piParadigmCollection = lemmatizerRu.CreateParadigmCollectionFromNorm(word, 0, 0);
+
+			for (int i = 0; i < piParadigmCollection.Count; ++i)
+			{
+				for (uint j = 0; j < piParadigmCollection[i].Count; j++)
+				{
+					yield return piParadigmCollection[i][j].ToLower();
+				}
+			}
 		}
 	}
 	
