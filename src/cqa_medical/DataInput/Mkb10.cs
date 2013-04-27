26105ed5402d9bbbe5887c398de159aeb3bb55d9
Alexander <bellal89@mail.ru>
2013-04-28 01:46:03 +0600

    Mkb10 trade names Fuzzy index works. Pairs retrieval works now.

--- src/cqa_medical/DataInput/Mkb10.cs
+++ src/cqa_medical/DataInput/Mkb10.cs
@@ -9,66 +9,55 @@ using cqa_medical.UtilitsNamespace;
 
 namespace cqa_medical.DataInput
 {
-	public static class Mkb10
+	public class Mkb10
 	{
-		const string DeseasesFileName = Program.FilesDirectory + "Mkb-10-Handy/Mkb10-handy-deseases.txt";
+		const string SerializedMappingFileName = Program.FilesDirectory + "Mkb-10-Handy/Mkb10-handy-deseases.txt";
 		const string DeseaseNamesFileName = Program.FilesDirectory + "Mkb-10-Handy/Mkb10-handy-deseaseNames.txt";
 		const string MedicamentNamesFileName = Program.FilesDirectory + "Mkb-10-Handy/Mkb10-handy-medicamentNames.txt";
 
-		private static readonly List<Question> MedicalQuestions;
+		private readonly List<Question> medicalQuestions;
+		
+		private readonly Dictionary<int, Mkb10Desease> idToDesease;
+		private readonly Dictionary<int, Mkb10TradeName> idToTradeName;
+		private readonly Dictionary<int, List<int>> desToTnMapping;
 
-		static Mkb10()
+		public Mkb10()
 		{
-			 MedicalQuestions =
-			Program.DefaultQuestionList.NewQuestionListFilteredByCategories("illness", "treatment", "kidhealth", "doctor").
-				GetAllQuestions().ToList();
-		}
+			medicalQuestions =
+				Program.DefaultQuestionList.NewQuestionListFilteredByCategories("illness", "treatment", "kidhealth", "doctor").
+					GetAllQuestions().ToList();
 
-		private static FuzzyIndex GetWordFuzzyIndex(IEnumerable<string> names)
-		{
-			var questionTexts = MedicalQuestions.Select(q => Tuple.Create(q.Id, q.WholeText));
+			desToTnMapping = ObjectSerializer.GetFromFile<Dictionary<int, List<int>>>(SerializedMappingFileName);
 
-			return new FuzzyIndex(questionTexts, names.SelectMany(name => name.SplitIntoWords()));
-		}
+			idToTradeName =
+				File.ReadAllLines(MedicamentNamesFileName).Select(line => line.Split('\t')).Where(parts => parts.Length > 1).ToDictionary
+					(parts => int.Parse(parts[0]), parts => new Mkb10TradeName(parts[1], parts.Length > 2 ? parts[2] : ""));
 
-		public static List<InvertedIndexUnit> GetDeseasesFuzzyIndex()
-		{
-			var deseases = LoadDeseases(DeseasesFileName, DeseaseNamesFileName, MedicamentNamesFileName);
-			var deseaseNames = deseases.Select(des => { var names = des.Synonyms; names.Add(des.Name);
-			                                          	return Tuple.Create(des.Name, names);
-			}).ToList();
-			
+			var deseaseParts =
+				File.ReadAllLines(DeseaseNamesFileName).Select(line => line.Split('\t')).Where(parts => parts.Length > 2).Select(
+					parts => Tuple.Create(int.Parse(parts[0]), parts)).ToList();
 
-			var stemmer = new MyStemmer("Mkb10Deseases.stemmed.txt", deseaseNames.SelectMany(it => it.Item2));
-			var stemmedDeseaseNames =
-				deseaseNames.Select(it => Tuple.Create(it.Item1, it.Item2.Select(name => String.Join(" ", name.SplitIntoWords().Select(stemmer.Stem))))).ToList();
+			var idDeseaseList =
+				deseaseParts.Where(it => it.Item2.Length > 3).Select(
+					it => Tuple.Create(it.Item1,
+					                   new Mkb10Desease(it.Item2[1],
+					                                    desToTnMapping[it.Item1].Where(idToTradeName.ContainsKey).Select(
+					                                    	id => idToTradeName[id]).ToList(),
+					                                    it.Item2[3].Split('+').ToList()))).ToList();
 
-			var wordsIndex = GetWordFuzzyIndex(stemmedDeseaseNames.SelectMany(it => it.Item2));
+			idDeseaseList.AddRange(
+				deseaseParts.Where(it => it.Item2.Length <= 3).Select(
+					it => Tuple.Create(it.Item1,
+					                   new Mkb10Desease(it.Item2[1], desToTnMapping[it.Item1].Select(id => idToTradeName[id]).ToList()))));
 
-			
-
-			var deseaseIndex = new List<InvertedIndexUnit>();
-
-			foreach (var it in stemmedDeseaseNames)
-			{
-				var desName = it.Item1;
-				var names = it.Item2;
-				var deseaseIds = new List<long>();
-				foreach (var name in names)
-				{
-					deseaseIds.AddRange(GetUnitsIntersection(wordsIndex.GetIndex(), name.SplitIntoWords().ToArray()));
-				}
-				if(deseaseIds.Any())
-					deseaseIndex.Add(new InvertedIndexUnit(desName, deseaseIds));
-			}
-			return deseaseIndex;
+			idToDesease = idDeseaseList.ToDictionary(it => it.Item1, it => it.Item2);
 		}
 
 		private static IEnumerable<long> GetUnitsIntersection(IEnumerable<InvertedIndexUnit> index, params string[] items)
 		{
 			var itemsSet = new HashSet<string>(items);
 			var units = index.Where(unit => itemsSet.Contains(unit.Word)).ToList();
-			
+
 			if (!itemsSet.Any() || itemsSet.Except(units.Select(unit => unit.Word)).Any())
 				return new HashSet<long>();
 
@@ -76,36 +65,91 @@ namespace cqa_medical.DataInput
 			return units.Skip(1).Aggregate(seed, (current, unit) => new HashSet<long>(current.Intersect(unit.Ids)));
 		}
 
-		private static IEnumerable<Mkb10Desease> LoadDeseases(string serializedMappingFileName, string deseaseNamesFileName, string tradeNamesFileName)
+		private IEnumerable<Tuple<int, InvertedIndexUnit>> GetDeseasesFuzzyIndex()
 		{
-			var mapping = ObjectSerializer.GetFromFile<Dictionary<int, List<int>>>(serializedMappingFileName);
-			var tradeNames =
-				File.ReadAllLines(tradeNamesFileName).Select(line => line.Split('\t')).Where(parts => parts.Length > 1).ToDictionary
-					(parts => int.Parse(parts[0]), parts => new Mkb10TradeName(parts[1], parts.Length > 2 ? parts[2] : ""));
+			var deseaseNames = idToDesease.Select(des => { var names = des.Value.Synonyms; names.Add(des.Value.Name);
+			                                          	return Tuple.Create(des.Key, names);
+			}).ToList();
+			
+			var stemmer = new MyStemmer("Mkb10Deseases.stemmed.txt", deseaseNames.SelectMany(it => it.Item2));
+			var stemmedDeseaseNames =
+				deseaseNames.Select(it => Tuple.Create(it.Item1, it.Item2.Select(name => String.Join(" ", name.SplitIntoWords().Select(stemmer.Stem))))).ToList();
 
-			var deseaseParts = File.ReadAllLines(deseaseNamesFileName).Select(line => line.Split('\t')).Where(parts => parts.Length > 2).Select(parts => Tuple.Create(int.Parse(parts[0]), parts)).ToList();
-			var deseasesList =
-				deseaseParts.Where(it => it.Item2.Length > 3).Select(
-					it =>
-					new Mkb10Desease(it.Item2[1], mapping[it.Item1].Where(tradeNames.ContainsKey).Select(id => tradeNames[id]).ToList(),
-					                              it.Item2[3].Split('+').ToList())).ToList();
+			var desWords = stemmedDeseaseNames.SelectMany(it => it.Item2).SelectMany(name => name.SplitIntoWords());
+			var questionTexts = medicalQuestions.Select(q => Tuple.Create(q.Id, q.WholeText));
+			var wordsIndex = new FuzzyIndex(questionTexts, desWords);
 
-			deseasesList.AddRange(
-				deseaseParts.Where(it => it.Item2.Length <= 3).Select(
-					it =>
-					new Mkb10Desease(it.Item2[1], mapping[it.Item1].Select(id => tradeNames[id]).ToList())));
-			return deseasesList;
+			var deseaseIndex = new List<Tuple<int, InvertedIndexUnit>>();
+
+			foreach (var it in stemmedDeseaseNames)
+			{
+				var desId = it.Item1;
+				var names = it.Item2;
+				var deseaseIds = new List<long>();
+				foreach (var name in names)
+				{
+					deseaseIds.AddRange(GetUnitsIntersection(wordsIndex.GetIndex(), name.SplitIntoWords().ToArray()));
+				}
+				if(deseaseIds.Any())
+					deseaseIndex.Add(Tuple.Create(desId, new InvertedIndexUnit(idToDesease[desId].Name, deseaseIds)));
+			}
+			return deseaseIndex;
+		}
+
+		private List<Tuple<int, InvertedIndexUnit>> GetMedicamentsFuzzyIndex()
+		{
+			var medNames = idToTradeName.Select(med => Tuple.Create(med.Key, med.Value.Name)).ToList();
+			var stemmer = new MyStemmer("Mkb10TradeNames.stemmed.txt", medNames.Select(it => it.Item2));
+			var stemmedTnNames =
+				medNames.Select(it => Tuple.Create(it.Item1, String.Join(" ", it.Item2.SplitIntoWords().Select(stemmer.Stem)))).ToList();
+
+			var tnWords = stemmedTnNames.Select(it => it.Item2).SelectMany(name => name.SplitIntoWords());
+			var answerTexts = medicalQuestions.SelectMany(q => q.GetAnswers()).Select(a => Tuple.Create(a.QuestionId, a.Text));
+			var wordsIndex = new FuzzyIndex(answerTexts, tnWords);
+
+			return (from it in medNames
+			        let medId = it.Item1
+			        let name = it.Item2
+			        let medIds = GetUnitsIntersection(wordsIndex.GetIndex(), name.SplitIntoWords().ToArray()).ToList()
+			        where medIds.Any()
+			        select Tuple.Create(medId, new InvertedIndexUnit(idToTradeName[medId].Name, medIds))).ToList();
+		}
+
+		public List<Tuple<int, int, int>> GetDesMedPairs()
+		{
+			var desIndex = GetDeseasesFuzzyIndex();
+			var medIndex = GetMedicamentsFuzzyIndex();
+			return (from med in medIndex
+			        from des in desIndex
+			        let intersection = des.Item2.Ids.Intersect(med.Item2.Ids).ToList()
+			        where intersection.Any()
+			        select Tuple.Create(des.Item1, med.Item1, intersection.Count)).ToList();
+		}
+
+		public IEnumerable<Tuple<int, int>> GetCorrectDesMedPairs()
+		{
+			return desToTnMapping.SelectMany(kv => kv.Value.Select(med => Tuple.Create(kv.Key, med)));
 		}
 	}
 
 	[TestFixture]
 	public class Mkb10Test
 	{
+		private readonly Mkb10 mkb = new Mkb10();
+
 		[Test]
-		public static void TeslLoading()
+		public void TestLoading()
 		{
-			var index = Mkb10.GetDeseasesFuzzyIndex();
-			Console.WriteLine(index.Count);
+			var correctPairs = new HashSet<Tuple<int, int>>(mkb.GetCorrectDesMedPairs());
+			Console.WriteLine("Correct pairs count: " + correctPairs.Count);
+
+			var pairs = mkb.GetDesMedPairs();
+			Console.WriteLine("Extracted pairs count: " + pairs.Count);
+
+			var correctExtracted = pairs.Where(pair => correctPairs.Contains(Tuple.Create(pair.Item1, pair.Item2))).ToList();
+			Console.WriteLine("Correct extracted count: " + correctExtracted.Count);
+			Console.WriteLine("Fraction of correct in extracted: " +  (double)correctExtracted.Count / pairs.Count);
+			Console.WriteLine("Sum fraction: " + (double)correctExtracted.Sum(it => it.Item3) / pairs.Sum(it => it.Item3));
 		}
 	}
 }
